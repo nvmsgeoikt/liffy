199c7e0cce0c7506c6a7e64347330ba9591cda45
Anders Gjerde <anders.gjerde@geomatikk-ikt.no>
2014-03-26 09:21:06 +0100

    Externalized javascript + added clicked here-function

---
+++ res/scripts.js
@@ -0,0 +1,16 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+//var map = L.map('map').setView([59.91545099, 10.73322757], 13);  //Ved slottsparken i Oslo
+
+//L.tileLayer('http://wms.geonorge.no/skwms1/wms.topo2?GetMap',
+L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
+        {
+            attribution: 'Map data &copy; <a href="http://www.geonorge.no">GeoNorge</a>',
+            maxZoom: 18
+        }
+).addTo(map);
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
