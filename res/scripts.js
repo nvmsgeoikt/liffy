29819dcfa9b69b82942cb65b87bb1ac80ed0ed1d
Anders Gjerde <anders.gjerde@geomatikk-ikt.no>
2014-03-26 10:27:43 +0100

    Added geonorge as wms, positioned map over Oslo

--- res/scripts.js
+++ res/scripts.js
@@ -1,9 +1,11 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
-//var map = L.map('map').setView([59.91545099, 10.73322757], 13);  //Ved slottsparken i Oslo
+//var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([59.91545099, 10.73322757], 13);  //Ved slottsparken i Oslo
 
-//L.tileLayer('http://wms.geonorge.no/skwms1/wms.topo2?GetMap',
-L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
+L.tileLayer.wms('http://wms.geonorge.no/skwms1/wms.topo2?GetMap',
+//L.tileLayer('http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
         {
+			layers: 'N250Hoydelag',
+			format: 'image/png',
             attribution: 'Map data &copy; <a href="http://www.geonorge.no">GeoNorge</a>',
             maxZoom: 18
         }
